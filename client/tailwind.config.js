aae51b34d91297894dfcdac71ad99bf99270061f
Duy Tien <tbuiduy2212@gmail.com>
2025-03-10 19:24:02 +0700

    set up base

---
+++ client/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}", "./public/index.html"],
+  theme: {
+    fontFamily:{
+      main:['Poppins','sans-serif;']
+    },
+    extend: {
+      width:{
+        main:'1220px'
+      },
+      backgroundColor:{
+        main:'#ee3131'
+      },
+      colors:{
+        main:'#ee3131'
+      }
+    },
+  },
+  plugins: [],
+};
