7d74b8e54d0f8336b25044fafba03dbe5690f6e4
Duy Tien <tbuiduy2212@gmail.com>
2025-03-12 22:10:56 +0700

    layout admin

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,48 @@
-import React,{useEffect} from "react";
-import { Route, Routes } from 'react-router-dom';
-import { Login, Home, Public } from "./pages/public";
-import path from './ultils/path';
-import {getCategories} from './store/asynActions'
-import {useDispatch} from 'react-redux'
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Login, Home, Public } from "pages/public";
+import {
+  AdminLayout,
+  Dashboard,
+  ManageOrder,
+  ManageProducts,
+  ManageUser,
+  CreateProducts,
+} from "pages/admin";
+import { MemberLayout, Personal } from "pages/member";
+import path from "ultils/path";
+import { getCategories } from "store/asynActions";
+import { useDispatch } from "react-redux";
+import { Dashboard } from "pages/admin";
+import { Modal } from "components";
+
 function App() {
   const dispatch = useDispatch();
-  useEffect(()=>{
-    dispatch(getCategories())
-  },[])
+  useEffect(() => {
+    dispatch(getCategories());
+  }, []);
   return (
     <div className="min-h-screen font-main">
       <Routes>
         <Route path={path.PUBLIC} element={<Public />}>
           <Route path={path.HOME} element={<Home />} />
           <Route path={path.LOGIN} element={<Login />} />
+          <Route path={path.ALL} element={<Home />} />
         </Route>
+        <Route path={path.ADMIN} element={<AdminLayout />}>
+          <Route path={path.DASHBOARD} element={<Dashboard />} />
+          <Route path={path.MANAGE_ORDER} element={<ManageOrder />} />
+          <Route path={path.MANAGE_PRODUCTS} element={<ManageProducts />} />
+          <Route path={path.MANAGE_USER} element={<ManageUser />} />
+          <Route path={path.CREATE_PRODUCTS} element={<CreateProducts />} />
+        </Route>
+        <Route path={path.MEMBER} element={<MemberLayout />}>
+          <Route path={path.PERSONAL} element={<Personal />} />
+        </Route>
+        {/* Trên phần path.FINAL_REGISTER */}
+
+        {/* REGITER */}
+        {/* end register */}
       </Routes>
     </div>
   );
--- client/src/ultils/path.js
+++ client/src/ultils/path.js
@@ -7,5 +7,20 @@ const path = {
     BLOGS: 'blogs',
     OUR_SERVICES: 'services',
     FAQ: 'faqs',
+
+
+
+    // Admin
+    ADMIN: 'admin',
+    DASHBOARD: 'dashboard',
+    MANAGE_USER: 'manage-user',
+    MANAGE_PRODUCTS: 'manage-products',
+    MANAGE_ORDER: 'manage-order',
+    CREATE_PRODUCTS: 'create-products',
+
+    // Member 
+    MEMBER: 'member',
+    PERSONAL:'personal'
+
 }
 export default path
